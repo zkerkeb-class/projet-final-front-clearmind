e89c13a484da4289bc4364bb40b7a1fcf2458a36
Subjectory <ineozz_@outlook.fr>
2026-02-23 22:53:24 +0100

    responsive update

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import api from '../../api/axios';
 import { Lock, Mail, User, Upload, Terminal, AlertTriangle, Check, Image, ChevronLeft } from 'lucide-react';
